2b642d2a3bdefc8a42d0520e91b988c5abb5f955
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2023-01-27 19:11:06 +0900

    Added some comment loosely pointing to future development plans, fixed imports

--- slack_export_csv_converter/convert.py
+++ slack_export_csv_converter/convert.py
@@ -2,3 +2,17 @@ print("Hello world!")
 
 def return_hello():
   return "hello"
+
+# expect a path
+# use the path to discover channels folder and its files
+# for each channel, load all the files
+#   concatenate the files
+#   perform data conversion
+#   output to csv
+
+# what components should there be
+# component that deals with directory structure
+# component that parses/reads the files
+# component that delas with data conversion -> directory
+# component that writes object -> directory, 
+# logger
--- slack_export_csv_converter/tests/convert_test.py
+++ slack_export_csv_converter/tests/convert_test.py
@@ -1,4 +1,4 @@
-from ..convert import return_hello
+from slack_export_csv_converter.convert import return_hello
 
 def test_hello():
   assert return_hello() == "hello"
