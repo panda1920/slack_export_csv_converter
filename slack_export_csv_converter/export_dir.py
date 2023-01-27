1ba593b793af60c2444a0cc8e60c1e16bfafe77c
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2023-01-28 03:47:45 +0900

    Finished implementing export_dir

--- slack_export_csv_converter/export_dir.py
+++ slack_export_csv_converter/export_dir.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 from slack_export_csv_converter.exceptions import ConverterException
 
@@ -28,6 +28,32 @@ class ExportDir:
       dir for dir in self._export_dir.iterdir() if dir.is_dir()
     ]
 
-  def _check_exists(self, path: Path) -> None:
+  def get_channel_path(self, channel: str) -> Path:
+    """
+    A utility function to retrieve channel path from name
+    """
+    channel_paths = self.get_channel_dirs()
+    found_paths = [ path for path in channel_paths if path.stem == channel ]
+    
+    if len(found_paths) < 1:
+      raise ConverterException(f"channel {channel} does not exist")
+    
+    return found_paths[0]
+
+  def get_message_files(self, channel: str) -> List[Path]:
+    """
+    Get message files belonging to a channel
+    """
+    channel_path = self._export_dir / channel
+    self._check_exists(channel_path, f"channel {channel} does not exist")
+    return [
+      file for file in channel_path.iterdir()
+      if file.is_file() and file.suffix == ".json"
+    ]
+
+  def _check_exists(self, path: Path, fail_msg: Optional[str] = None) -> None:
+    if fail_msg == None:
+      fail_msg = f'path {str(path)} does not exist'
+    
     if not path.exists():
-      raise ConverterException(f'path {str(path)} does not exist')
+      raise ConverterException(fail_msg)
--- slack_export_csv_converter/tests/export_dir_test.py
+++ slack_export_csv_converter/tests/export_dir_test.py
@@ -1,6 +1,6 @@
-from pathlib import Path
-
 import pytest
+from pathlib import Path
+from typing import List
 
 from slack_export_csv_converter.export_dir import ExportDir
 from slack_export_csv_converter.exceptions import ConverterException
@@ -16,6 +16,16 @@ class TestExportDir:
     'channel 三'
   ]
 
+  @pytest.fixture(scope="function")
+  def create_channels(self, tmp_path: Path):
+    channel_paths = []
+    for channel in self.TEST_CHANNELS:
+      channel_path = tmp_path / channel
+      channel_path.mkdir()
+      channel_paths.append(channel_path)
+
+    return channel_paths
+
   def should_accept_path_ctor(self, tmp_path):
     ExportDir(tmp_path)
 
@@ -26,38 +36,114 @@ class TestExportDir:
       ExportDir(nonexisting)
 
   def should_return_users_file_path(self, export_dir: ExportDir, tmp_path: Path):
-      expected_users_file = tmp_path / ExportDir.USERS_FILE_NAME
-      expected_users_file.touch()
+    expected_users_file = tmp_path / ExportDir.USERS_FILE_NAME
+    expected_users_file.touch()
 
-      users_file = export_dir.get_users_file()
+    users_file = export_dir.get_users_file()
 
-      assert expected_users_file == users_file
+    assert users_file == expected_users_file
 
   def should_throw_when_users_file_not_exist(self, export_dir: ExportDir):
     with pytest.raises(ConverterException):
       export_dir.get_users_file()
 
   def should_return_channel_dirs(self, export_dir: ExportDir, tmp_path: Path):
-      expected_channel_dirs = [
-        tmp_path / channel for channel in self.TEST_CHANNELS
-      ]
-      for dir in expected_channel_dirs:
-        dir.mkdir()
+    expected_channel_dirs = [
+      tmp_path / channel for channel in self.TEST_CHANNELS
+    ]
+    for dir in expected_channel_dirs:
+      dir.mkdir()
 
-      channel_dirs = export_dir.get_channel_dirs()
-      assert len(expected_channel_dirs) == len(channel_dirs)
-      for dir in channel_dirs:
-        assert dir in expected_channel_dirs
+    channel_dirs = export_dir.get_channel_dirs()
+
+    assert len(channel_dirs) == len(expected_channel_dirs)
+    for dir in channel_dirs:
+      assert dir in expected_channel_dirs
 
   def should_return_empty_list_when_no_channel(self, export_dir: ExportDir):
-      expected_channel_dirs = []
+    expected_channel_dirs = []
 
-      channel_dirs = export_dir.get_channel_dirs()
-      assert len(expected_channel_dirs) == len(channel_dirs)
-      assert expected_channel_dirs == channel_dirs
-  
-  # should return multiple files for messages
-  # should return 0 file when channel empty
-  # should return only json files
-  # should throw when not exist
-  # should throw when channel not exist
+    channel_dirs = export_dir.get_channel_dirs()
+
+    assert len(channel_dirs) == len(expected_channel_dirs)
+    assert channel_dirs == expected_channel_dirs
+
+  def should_return_messages_for_channel(self, export_dir: ExportDir, create_channels: List[Path]):
+    channel_1 = create_channels[0].stem
+    channel_2 = create_channels[1].stem
+
+    # setup files for channel_1
+    files_1 = [
+      create_channels[0] / '1.json',
+      create_channels[0] / '2.json',
+      create_channels[0] / '3.json'
+    ]
+    # setup files for channel_2
+    files_2 = [
+      create_channels[1] / '11.json',
+      create_channels[1] / '12.json',
+      create_channels[1] / '13.json',
+      create_channels[1] / '14.json',
+      create_channels[1] / '15.json',
+    ]
+    for file in [*files_1, *files_2]:
+      file.touch()
+    
+    messages = export_dir.get_message_files(channel_1)
+
+    assert len(messages) == len(files_1)
+    for file in messages:
+      assert file in files_1
+
+    messages = export_dir.get_message_files(channel_2)
+
+    assert len(messages) == len(files_2)
+    for file in messages:
+      assert file in files_2
+
+  def should_only_return_json_files_for_message(self, export_dir: ExportDir, create_channels: List[Path]):
+    channel = create_channels[0].stem
+
+    # setup files for channel
+    json_files = [
+      create_channels[0] / '1.json',
+      create_channels[0] / '2.json',
+      create_channels[0] / '3.json'
+    ]
+    non_json_files = [
+      create_channels[0] / 'data.csv',
+      create_channels[0] / 'some.log',
+      create_channels[0] / 'notes.txt',
+      create_channels[0] / 'asset.png'
+    ]
+    for file in [*json_files, *non_json_files]:
+      file.touch()
+
+    messages = export_dir.get_message_files(channel)
+
+    assert len(messages) == len(json_files)
+    for file in messages:
+      assert file in json_files
+
+  def should_return_no_messages_when_channel_empty(self, export_dir: ExportDir, create_channels: List[Path]):
+    channel_1, channel_2, channel_3 = create_channels
+
+    assert len(export_dir.get_message_files(channel_1)) == 0
+    assert len(export_dir.get_message_files(channel_2)) == 0
+    assert len(export_dir.get_message_files(channel_3)) == 0
+
+  def should_throw_when_get_message_channel_not_exist(self, export_dir: ExportDir, create_channels: List[Path]):
+    with pytest.raises(ConverterException):
+      export_dir.get_message_files("NON_EXISTANT_CHANNEL")
+
+  def should_get_channel_path_from_name(self, export_dir: ExportDir, create_channels: List[Path]):
+    for expected_channel_path in create_channels:
+      channel_name = expected_channel_path.stem
+
+      channel_path = export_dir.get_channel_path(channel_name)
+      
+      assert channel_path == expected_channel_path
+
+  def should_throw_when_get_channel_path_not_found(self, export_dir: ExportDir, create_channels: List[Path]):
+    with pytest.raises(ConverterException):
+      export_dir.get_channel_path("NON_EXISTANT_CHANNEL")
