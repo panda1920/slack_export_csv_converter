6e6a327a65ff9b9ed9eea9a57cfa89f8d7ecaa80
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2023-02-17 18:07:17 +0900

    Implemented main.py

--- main.py
+++ main.py
@@ -1,28 +1,65 @@
 # -*- coding: utf-8 -*-
+import sys
+from pathlib import Path
+from typing import List, cast
+import logging
+import os
+
 from slack_export_csv_converter.logger import setup_logger
+from slack_export_csv_converter.export_dir import ExportDir
+from slack_export_csv_converter.file_io import FileIO
+from slack_export_csv_converter.csv_data_generator import CSVDataGenerator
+from slack_export_csv_converter.converter import Converter
+from slack_export_csv_converter.types import ExportFileContent
+from slack_export_csv_converter.exceptions import ConverterException
 
 
-def return_hello():
-    return "hello"
+def main(args):
+    try:
+        setup_logger()
+        path_args = convert_args_to_path(validate_args(sanitize_args(args)))
+        converter = setup_converter(path_args)
+        converter.run()
+    except Exception as e:
+        logging.critical(repr(e))
+        exit(1)
 
 
-def main():
-    setup_logger()
+def sanitize_args(args: List[str]) -> List[str]:
+    sanitized_args = []
+    for arg in args:
+        sanitized_arg = arg.strip()
+        if sanitized_arg:
+            sanitized_args.append(sanitized_arg)
+
+    return sanitized_args
+
+
+def validate_args(args: List[str]) -> List[str]:
+    if len(args) < 1:
+        raise ConverterException("有効なパスを指定してください。")
+    if len(args) > 2:
+        raise ConverterException("引数が多すぎます。有効なパスを1つまたは2つ指定してください。")
+    if len(args) == 1:
+        args.append(os.getcwd())
+
+    return args
+
+
+def convert_args_to_path(args: List[str]) -> List[Path]:
+    return [Path(arg) for arg in args]
+
+
+def setup_converter(paths: List[Path]) -> Converter:
+    export_dir = ExportDir(*paths)
+    file_io = FileIO()
+    users_file_content = cast(
+        ExportFileContent, file_io.read_json(export_dir.get_users_file())
+    )
+    csv_data_generator = CSVDataGenerator(users_file_content)
+
+    return Converter(export_dir, file_io, csv_data_generator)
 
 
 if __name__ == "__main__":
-    main()
-
-# expect a path
-# use the path to discover channels folder and its files
-# for each channel, load all the files
-#   concatenate the files
-#   perform data conversion
-#   output to csv
-
-# what components should there be
-# component that deals with directory structure
-# component that reads/writes file
-# component that deals with message data conversion
-# component that deals with attachment data generation/conversion?
-# logger
+    main(sys.argv[1:])
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,5 +1,151 @@
-from main import return_hello
+import pytest
+from unittest.mock import patch, sentinel
+from contextlib import contextmanager
+from pathlib import Path
+
+from main import main
+from slack_export_csv_converter.exceptions import ConverterException
 
 
-def test_hello():
-    assert return_hello() == "hello"
+TEST_PATH_1 = "/path/to/export"
+TEST_PATH_2 = "/path/to/save/location"
+
+
+class TestMain:
+    @contextmanager
+    def patch_dependencies(self):
+        # fmt: off
+        with patch("main.ExportDir") as export_dir, \
+                patch("main.FileIO") as file_io, \
+                patch("main.CSVDataGenerator") as csv_data_generator, \
+                patch("main.Converter") as converter:
+            # fmt: on
+            yield (export_dir, file_io, csv_data_generator, converter)
+
+    def shouldInitializeObjects(self):
+        with self.patch_dependencies() as patches:
+            (export_dir, file_io, csv_data_generator, converter) = patches
+
+            main([TEST_PATH_1, TEST_PATH_2])
+
+            export_dir.assert_called_once()
+            file_io.assert_called_once()
+            csv_data_generator.assert_called_once()
+            converter.assert_called_once()
+
+            # validate that components are built from other components
+            args = converter.call_args.args
+            assert args[0] is export_dir()
+            assert args[1] is file_io()
+            assert args[2] is csv_data_generator()
+
+    def shouldPassArgumentToExportDir(self):
+        with self.patch_dependencies() as patches:
+            (export_dir, *_) = patches
+
+            main([TEST_PATH_1, TEST_PATH_2])
+
+            export_dir.assert_called_with(Path(TEST_PATH_1), Path(TEST_PATH_2))
+
+    def shouldPassCwdToExportDirWhenSecondArgNotPresent(self):
+        with self.patch_dependencies() as patches:
+            (export_dir, *_) = patches
+
+            main([TEST_PATH_1])
+
+            export_dir.assert_called_with(Path(TEST_PATH_1), Path.cwd())
+
+    def shouldSanitizeArgument(self):
+        with self.patch_dependencies() as patches:
+            (export_dir, *_) = patches
+
+            main([TEST_PATH_1 + " ", TEST_PATH_2 + " "])
+
+            export_dir.assert_called_with(Path(TEST_PATH_1), Path(TEST_PATH_2))
+            export_dir.reset_mock()
+
+            main([" " + TEST_PATH_1, " " + TEST_PATH_2])
+
+            export_dir.assert_called_with(Path(TEST_PATH_1), Path(TEST_PATH_2))
+            export_dir.reset_mock()
+
+            main([TEST_PATH_1, " "])
+
+            export_dir.assert_called_with(Path(TEST_PATH_1), Path.cwd())
+            export_dir.reset_mock()
+
+            main([" ", TEST_PATH_2])
+
+            export_dir.assert_called_with(Path(TEST_PATH_2), Path.cwd())
+
+    def shouldPassUsersFileContentToGenerator(self):
+        with self.patch_dependencies() as patches:
+            (export_dir, file_io, csv_data_generator, _) = patches
+            export_dir().get_users_file.return_value = sentinel.users_file
+            file_io().read_json.return_value = sentinel.users_file_content
+
+            main([TEST_PATH_1])
+
+            export_dir().get_users_file.assert_called_once()
+            file_io().read_json.assert_called_once_with(sentinel.users_file)
+            csv_data_generator.assert_called_once_with(sentinel.users_file_content)
+
+    def shouldRunConverter(self):
+        with self.patch_dependencies() as patches:
+            (_, _, _, converter) = patches
+
+            main([TEST_PATH_1])
+
+            converter().run.assert_called_once()
+
+    def shouldExitAndNotRunConverterWhenNoArguments(self):
+        with self.patch_dependencies() as patches:
+            (_, _, _, converter) = patches
+
+            with pytest.raises(SystemExit):
+                main([])
+
+            converter().run.assert_not_called()
+
+    def shouldExitAndNotRunConverterWhenTooMuchArguments(self):
+        with self.patch_dependencies() as patches:
+            (_, _, _, converter) = patches
+
+            with pytest.raises(SystemExit):
+                main([TEST_PATH_1, TEST_PATH_2, "123"])
+                main([TEST_PATH_1, TEST_PATH_2, "123", "hello", "world"])
+
+            converter().run.assert_not_called()
+
+    def shouldExitWhenException(self):
+        with self.patch_dependencies() as patches:
+            (export_dir, file_io, csv_data_generator, converter) = patches
+
+            export_dir.side_effect = ConverterException("Something")
+
+            with pytest.raises(SystemExit):
+                main([TEST_PATH_1, TEST_PATH_2])
+
+            export_dir.reset_mock(side_effect=True)
+            file_io.side_effect = ConverterException("Something")
+
+            with pytest.raises(SystemExit):
+                main([TEST_PATH_1, TEST_PATH_2])
+
+            file_io.reset_mock(side_effect=True)
+            csv_data_generator.side_effect = ConverterException("Something")
+
+            with pytest.raises(SystemExit):
+                main([TEST_PATH_1, TEST_PATH_2])
+
+            csv_data_generator.reset_mock(side_effect=True)
+            converter.side_effect = ConverterException("Something")
+
+            with pytest.raises(SystemExit):
+                main([TEST_PATH_1, TEST_PATH_2])
+
+            converter.reset_mock(side_effect=True)
+            converter().run.side_effect = ConverterException("Something")
+
+            with pytest.raises(SystemExit):
+                main([TEST_PATH_1, TEST_PATH_2])
