62905e11cb32871f70e47c1ac1be28ce89b3051a
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2023-02-18 12:59:52 +0900

    Updated logger.py so that it sets up 2 type of handlers

--- slack_export_csv_converter/logger.py
+++ slack_export_csv_converter/logger.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import logging
+from pathlib import Path
 
 
 def setup_logger() -> None:
     """Sets up root logger of python standard library
 
-    This function defines how to format and where to send the messages.
-    After calling this function just use logging.debug() and the like to log messages.
+    This function defines how to format and where to send the logs.
+    After calling this function just use logging.debug() and the like throughout the code.
 
     Args:
 
@@ -16,9 +17,17 @@ def setup_logger() -> None:
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
 
-    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
-
     stderr_handler = logging.StreamHandler()
-    stderr_handler.setFormatter(formatter)
+    stderr_formatter = logging.Formatter("[%(levelname)s] %(message)s")
+    stderr_handler.setFormatter(stderr_formatter)
     stderr_handler.setLevel(logging.INFO)
     logger.addHandler(stderr_handler)
+
+    project_path = Path(__file__).resolve().parents[1]
+    file_handler = logging.FileHandler(
+        str(project_path / "slack_export_csv_converter.log"), encoding="utf-8"
+    )
+    file_formatter = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
